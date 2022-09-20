13b4a01e99e23a1c1d732c42a98fff523e0b3bb3
Habeebability <awoyemi.habeeb@gmail.com>
2022-09-20 15:03:26 +0100

    paginate

--- src/views/items/Items.vue
+++ src/views/items/Items.vue
@@ -119,83 +119,6 @@
         </div>
       </div>
     </div>
-    <!-- <div class="flex justify-center pagination">
-      <ul class="inline-flex items-center -space-x-px">
-        <li>
-          <a
-            href="#"
-            class="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span class="sr-only">Previous</span>
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >1</a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >2</a>
-        </li>
-        <li>
-          <a
-            href="#"
-            aria-current="page"
-            class="z-10 py-2 px-3 leading-tight text-blue-600 bg-blue-50 border border-blue-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-          >3</a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >4</a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >5</a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="block py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span class="sr-only">Next</span>
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </a>
-        </li>
-      </ul>
-    </div>-->
 
     <div v-if="selectItemsOption" class="flex justify-around">
       <div>
@@ -298,9 +221,6 @@
               <td>{{item.item_name}}</td>
               <td>{{item.store_name}}</td>
               <td class="text-center">
-                <!-- :v-model="item.quantity" -->
-                <!-- @change.prevent="checkQuantity(item)" -->
-                <!-- :id="item.item_id" -->
                 <input
                   v-model="item.selectedQuantity"
                   class="min-w-10 h-5 px-3 border border-primary mx-auto cursor-pointer"
@@ -309,7 +229,6 @@
                   :max="item.quantity"
                   @keydown="onKeydown"
                 />
-                <!-- :disabled="item.quantity > initialQuantity" -->
 
                 <i
                   @click="removeChecked(index)"
@@ -486,7 +405,7 @@ export default {
       try {
         isLoading.value = true;
         const response = await axios.get(`/api/v1/inventory`);
-        const allItems = response.data.items;
+        const allItems = response.data.data;
 
         allItemsList.value = allItems;
 
