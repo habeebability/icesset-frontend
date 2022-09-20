be354dba3e5cefaacf2898356014ebaa1e092f02
TolaniNuel2 <tolaninuel2@gmail.com>
2022-09-20 14:31:46 +0100

    Report page

--- src/views/reports/Report.vue
+++ src/views/reports/Report.vue
@@ -1,13 +1,21 @@
 <template>
   <h1 class="font-semibold lg:text-2xl text-sm my-3 ml-5">Report Page</h1>
   <div class="md:w-full w-screen">
-    <iframe
-    class="overflow-fill"
+    <!-- <iframe
+      class="overflow-fill"
       src="http://localhost:3000/public/dashboard/74674010-16c3-48e9-864d-59326766b685"
       frameborder="0"
       width="1400"
       height="2100"
-      allowtransparency></iframe>
+      allowtransparency
+    ></iframe> -->
+    <iframe
+      src="http://64.227.122.155:3000/public/dashboard/f0509eb2-9535-42d6-9520-01e8c5dd7b3e"
+      frameborder="0"
+      width="1400"
+      height="900"
+      allowtransparency
+    ></iframe>
   </div>
 </template>
 
