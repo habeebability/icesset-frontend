62a4b64e79bd8a472b980bf6b32682b1706e931f
Habeebability <awoyemi.habeeb@gmail.com>
2022-09-23 10:21:39 +0100

    fix: report page iframes

--- src/views/reports/Report.vue
+++ src/views/reports/Report.vue
@@ -1,51 +1,49 @@
 <template>
   <div class="ml-5">
-    
-
     <iframe
-      src="test.metabase.astrapay.com.ng/public/dashboard/f0509eb2-9535-42d6-9520-01e8c5dd7b3e"
+      src="https://test.metabase.astrapay.com.ng/public/dashboard/f0509eb2-9535-42d6-9520-01e8c5dd7b3e"
       frameborder="0"
       width="900"
       height="700"
       allowtransparency
     ></iframe>
     <iframe
-      src="test.metabase.astrapay.com.ng/public/dashboard/f00e26ac-50b2-4780-ac60-392b7240c704"
+      src="https://test.metabase.astrapay.com.ng/public/dashboard/f00e26ac-50b2-4780-ac60-392b7240c704"
       frameborder="0"
       width="800"
       height="600"
       allowtransparency
     ></iframe>
     <iframe
-      src="test.metabase.astrapay.com.ng/public/dashboard/dcc7dadf-97d4-4997-8615-1b15b54a653f"
+      src="https://test.metabase.astrapay.com.ng/public/dashboard/dcc7dadf-97d4-4997-8615-1b15b54a653f"
       frameborder="0"
       width="800"
       height="600"
       allowtransparency
     ></iframe>
     <iframe
-      src="test.metabase.astrapay.com.ng/public/dashboard/d277c038-e92e-47d1-ae5a-9fe55faca272"
+      src="https://test.metabase.astrapay.com.ng/public/dashboard/d277c038-e92e-47d1-ae5a-9fe55faca272"
       frameborder="0"
       width="800"
       height="600"
       allowtransparency
     ></iframe>
     <iframe
-      src="test.metabase.astrapay.com.ng/public/dashboard/8c6f2f84-fbb2-4c9d-b8a3-819113c9b5a9"
+      src="https://test.metabase.astrapay.com.ng/public/dashboard/8c6f2f84-fbb2-4c9d-b8a3-819113c9b5a9"
       frameborder="0"
       width="800"
       height="600"
       allowtransparency
     ></iframe>
     <iframe
-      src="test.metabase.astrapay.com.ng/public/dashboard/8c3fe47f-e701-4557-bc92-b35d1ff9b7d1"
+      src="https://test.metabase.astrapay.com.ng/public/dashboard/8c3fe47f-e701-4557-bc92-b35d1ff9b7d1"
       frameborder="0"
       width="800"
       height="600"
       allowtransparency
     ></iframe>
     <iframe
-      src="test.metabase.astrapay.com.ng/public/dashboard/cb33959e-e14e-4453-9c22-ca24475b5f3f"
+      src="https://test.metabase.astrapay.com.ng/public/dashboard/cb33959e-e14e-4453-9c22-ca24475b5f3f"
       frameborder="0"
       width="800"
       height="600"
