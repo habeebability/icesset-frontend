7b59d0745c7f5d7ecc175b97295d149d0308eca5
TolaniNuel2 <tolaninuel2@gmail.com>
2022-09-23 09:29:42 +0100

    Report page

--- src/views/reports/Report.vue
+++ src/views/reports/Report.vue
@@ -16,6 +16,48 @@
       height="700"
       allowtransparency
     ></iframe>
+    <iframe
+      src="http://64.227.122.155:3000/public/dashboard/f00e26ac-50b2-4780-ac60-392b7240c704"
+      frameborder="0"
+      width="800"
+      height="600"
+      allowtransparency
+    ></iframe>
+    <iframe
+      src="http://64.227.122.155:3000/public/dashboard/dcc7dadf-97d4-4997-8615-1b15b54a653f"
+      frameborder="0"
+      width="800"
+      height="600"
+      allowtransparency
+    ></iframe>
+    <iframe
+      src="http://64.227.122.155:3000/public/dashboard/d277c038-e92e-47d1-ae5a-9fe55faca272"
+      frameborder="0"
+      width="800"
+      height="600"
+      allowtransparency
+    ></iframe>
+    <iframe
+      src="http://64.227.122.155:3000/public/dashboard/8c6f2f84-fbb2-4c9d-b8a3-819113c9b5a9"
+      frameborder="0"
+      width="800"
+      height="600"
+      allowtransparency
+    ></iframe>
+    <iframe
+      src="http://64.227.122.155:3000/public/dashboard/8c3fe47f-e701-4557-bc92-b35d1ff9b7d1"
+      frameborder="0"
+      width="800"
+      height="600"
+      allowtransparency
+    ></iframe>
+    <iframe
+      src="http://64.227.122.155:3000/public/dashboard/cb33959e-e14e-4453-9c22-ca24475b5f3f"
+      frameborder="0"
+      width="800"
+      height="600"
+      allowtransparency
+    ></iframe>
   </div>
 </template>
 
